fcc1cd3e647b8c782aa957df296ec689bb96aee2
Kevin Heath <kheath@g.hmc.edu>
2014-10-30 14:58:07 -0700

    Merged my working version of the SafetyApp with Robin's version with settings.  Still need to remove the redundancy

--- SMSMessaging/app/src/main/java/myfirstapp/example/com/sms/SMS.java
+++ SMSMessaging/app/src/main/java/myfirstapp/example/com/sms/SMS.java
@@ -2,19 +2,24 @@
 
 package myfirstapp.example.com.sms;
 
-import myfirstapp.example.com.sms.GPSTracker;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.SmsManager;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView.BufferType;
 import android.widget.Toast;
-import android.view.Gravity;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SMS extends Activity
 {
@@ -37,6 +42,8 @@ public class SMS extends Activity
         txtMessage = (EditText) findViewById(R.id.txtMessage);
      // create GPS object
         gps = new GPSTracker(SMS.this);
+        
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
         btnSendSMS.setOnClickListener(new View.OnClickListener()
         {
@@ -66,7 +73,7 @@ public class SMS extends Activity
                 {
                 	message = "Yarr matey, I be in a bit of a pickle.";
                 }
-                txtMessage.setText(message);
+//                txtMessage.setText(message);
                 message = message + "\n Me location is - \nLat: " + latitude + "\nLong: " + longitude;
                 
                 if (isValidPhoneNumber(phoneNo))
@@ -133,6 +140,37 @@ public class SMS extends Activity
       	  System.out.println("Not a valid phone number!");
         }
         return isValid;
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sm, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        if (item.getItemId() == R.id.action_settings) {
+            // Starts the Settings activity on top of the current activity
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String newPhoneNo = preferences.getString("pref_recipientNumber", "");
+        String newMessage = preferences.getString("pref_message", "");
+        txtPhoneNo.setText(newPhoneNo, BufferType.NORMAL);
+        txtMessage.setText(newMessage, BufferType.NORMAL);
     }
     
 }
